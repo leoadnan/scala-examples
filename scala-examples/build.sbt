6e708c48d9ba68b3704f693f9a6d678799a6a45b
Adnan Ahmed <leo_adnan@hotmail.com>
2018-06-30 00:12:54 -0700

    update

--- scala-examples/build.sbt
+++ scala-examples/build.sbt
@@ -13,6 +13,8 @@ libraryDependencies ++= Seq(
 	"com.typesafe.akka" %% "akka-actor"          % akkaVersion withSources() withJavadoc(),
 	"com.typesafe.akka" %% "akka-stream"         % akkaVersion withSources() withJavadoc(),
 	
+	"com.typesafe.akka" %% "akka-http-spray-json" % "10.1.3" withSources() withJavadoc(),
+	
     "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % "test" withSources() withJavadoc(),
     "com.typesafe.akka" %% "akka-testkit"        % akkaVersion % "test" withSources() withJavadoc(),
     "org.scalatest"     %% "scalatest"           % "3.0.5"     % "test" withSources() withJavadoc()
---
+++ scala-examples/src/main/scala/stream/EventMarshalling.scala
@@ -0,0 +1,29 @@
+package stream
+
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import spray.json._
+
+trait EventMarshalling extends DefaultJsonProtocol {
+  implicit val dateTimeFormat = new JsonFormat[ZonedDateTime] {
+    def write(dateTime: ZonedDateTime) = JsString(dateTime.format(DateTimeFormatter.ISO_INSTANT))
+    def read(value: JsValue) = value match {
+      case JsString(str) =>
+        try {
+          ZonedDateTime.parse(str)
+        } catch {
+          case e: DateTimeParseException =>
+            val msg = s"Could not deserialize $str to ZonedDateTime"
+            deserializationError(msg)
+        }
+      case js =>
+        val msg = s"Could not deserialize $js to ZonedDateTime."
+        deserializationError(msg)
+    }
+  }
+
+  implicit val eventFormat = jsonFormat7(Event)
+  implicit val logIdFormat = jsonFormat2(LogReceipt)
+  implicit val errorFormat = jsonFormat2(ParseError)
+}
